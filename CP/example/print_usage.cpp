2d8cbbfdef1266acf6e9a886d8ed5d9553b88122
Govind Ji <jigovind556@gmail.com>
2024-12-07 00:03:53 +0530

    printing template for competitive programming

---
+++ CP/example/print_usage.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include "../debugging_codes/debugging.h"
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("CP/compile/Error.txt", "w", stderr);
+#endif
+    int a = 2;
+    int b = 3;
+    vector<char> c = {'a', 'b', 'c'};
+    vector<string> c1 = {"fdlk", "dlkaj", "dlsk"};
+    set<string> c2 = {"fdlk", "dlkaj", "dlsk"};
+
+    cin >> a;
+    cout << MOD;
+    debug(a);
+    debug(b);
+
+    debug(c);
+    debug(c1);
+    debug(c2);
+
+    return 0;
+}
